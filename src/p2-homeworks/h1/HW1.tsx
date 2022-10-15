85524f456050bbb6aa96535939abf16014166c55
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-10-15 23:08:21 +0300

    h1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -7,7 +7,7 @@ function HW1() {
     const messageData = {
         avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
         name: 'Иван',
-        message: 'npm start нажимал???',
+        message: 'npm start нажимал??????',
         time: '22:00',
     }
 
