7aca1f1f71951a36e9f6d262850a05da7de5c96a
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-10-31 12:47:12 +0300

    h3 difficult(((   complete 80%)))add onKeyDown

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
@@ -14,10 +14,13 @@ const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
     const inputClass = s.error // need to fix with (?:)
-
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter')
+            addUser()
+    }
     return (
         <div>
-            <input value={name} onChange={setNameCallback}
+            <input value={name} onChange={setNameCallback} onKeyDown={onKeyDownHandler}
                    className={inputClass} placeholder={'Введите ваше имя'}/>
             <span>{error}</span>
             <button onClick={addUser}>add</button>
