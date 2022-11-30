2080e04f5c863ab4847b588001e17f279d43a04e
Кирилл <94886658+Kiri1111@users.noreply.github.com>
2022-11-30 11:56:24 +0300

    hw8 complete 100%)))

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,10 +1,17 @@
 import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
+import {checkAC, homeWorkReducer, sortAC} from './bll/homeWorkReducer'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 // export type UserType =
+export type UserType = Array<ObjType>
 
-const initialPeople = [
+
+type ObjType = {
+    _id: number
+    name: string
+    age: number
+}
+const initialPeople: UserType = [
     {_id: 0, name: 'Кот', age: 3},
     {_id: 1, name: 'Александр', age: 66},
     {_id: 2, name: 'Коля', age: 16},
@@ -14,28 +21,35 @@ const initialPeople = [
 ]
 
 function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
+    const [people, setPeople] = useState<UserType>(initialPeople) // need to fix any
 
     // need to fix any
-    const finalPeople = people.map((p: any) => (
+    const finalPeople = people.map((p: ObjType) => (
         <div key={p._id}>
-            some name, age
+            <span>{'Name: ' + p.name}</span>
+            <span>{' Age: ' + p.age}</span>
         </div>
     ))
 
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
+    const sortUp = () => setPeople(homeWorkReducer(initialPeople, sortAC('up')))
+    const sortDown = () => setPeople(homeWorkReducer(initialPeople, sortAC('down')))
+    const check = () => setPeople(homeWorkReducer(initialPeople, checkAC(18)))
+    // const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
 
     return (
         <div>
             <hr/>
             homeworks 8
+            <hr/>
 
             {/*should work (должно работать)*/}
             {finalPeople}
 
             <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
+            <div><SuperButton onClick={sortDown}>sort down</SuperButton></div>
+            <div><SuperButton onClick={check}>check 18</SuperButton></div>
+            {/*<div>sort down</div>*/}
+            {/*check 18*/}
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,58 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType, action: AllActionTypes): UserType => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            let copyState = [...state]
+            if (action.payload === 'up') {
+
+                copyState.sort(function (a, b) {
+                        let nameA = a.name.toLowerCase()
+                        let nameB = b.name.toLowerCase()
+                        if (nameA < nameB)
+                            return -1
+                        if (nameA > nameB)
+                            return 1
+                        return 0
+                    }
+                )
+            }
+            if (action.payload === 'down') {
+
+                state.sort(function (a, b) {
+                        let nameA = a.name.toLowerCase()
+                        let nameB = b.name.toLowerCase()
+                        if (nameA > nameB)
+                            return -1
+                        if (nameA < nameB)
+                            return 1
+                        return 0
+                    }
+                )
+            }
+            return copyState
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(el => el.age > action.payload)
         }
-        default: return state
+        default:
+            return state
+    }
+}
+
+type AllActionTypes = sortACType | checkACType
+type sortACType = ReturnType<typeof sortAC>
+export const sortAC = (payload: string) => {
+    return {
+        type: 'sort',
+        payload
+    }
+}
+type checkACType = ReturnType<typeof checkAC>
+export const checkAC = (payload: number) => {
+    return {
+        type: 'check',
+        payload
     }
 }
